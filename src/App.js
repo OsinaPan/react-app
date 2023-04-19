8a4b6ba4c174d205a884062486d2ee4b567e1de2
Michal Osinski <michal.osinski94@gmail.com>
2023-04-19 19:03:58 +0200

    Problem with styles 15.4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Hero from './components/Hero/Hero'
 import SearchForm from './components/SearchForm/SearchForm'
-import List from './components/List/List.js';
+import List from './components/List/List';
 
 const App = () => {
   return (
